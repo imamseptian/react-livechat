af0987582d66a449410d70f596b1c95dc5ade256
imamseptian <imam.septian11187@gmail.com>
2021-08-04 23:35:26 +0700

    fix url

--- src/variables/MyVar.js
+++ src/variables/MyVar.js
@@ -1,6 +1,7 @@
-const expressURL = "http://localhost:4000/";
+const expressURL = "https://glacial-wave-49803.herokuapp.com/";
 // const socketURL = "http://localhost:8000/";
 // const socketURL = "http://localhost:8000/";
-const socketURL = "ws://localhost:8000";
+// const socketURL = "ws://localhost:8000";
+const socketURL = "ws://cryptic-dusk-85800.herokuapp.com";
 
 export { expressURL, socketURL };
