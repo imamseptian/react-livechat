33e4117ad3b55debdb7cad43d0379dc581bedb9c
imamseptian <imam.septian11187@gmail.com>
2021-08-04 23:42:29 +0700

    fix wsl

--- src/variables/MyVar.js
+++ src/variables/MyVar.js
@@ -2,6 +2,7 @@ const expressURL = "https://glacial-wave-49803.herokuapp.com/";
 // const socketURL = "http://localhost:8000/";
 // const socketURL = "http://localhost:8000/";
 // const socketURL = "ws://localhost:8000";
-const socketURL = "ws://cryptic-dusk-85800.herokuapp.com";
+// const socketURL = "ws://cryptic-dusk-85800.herokuapp.com";
+const socketURL = "wss://cryptic-dusk-85800.herokuapp.com";
 
 export { expressURL, socketURL };
